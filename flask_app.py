0ccd9ff2a9005feff3fcc0fb76af96f416f249d1
Jeffrey Robbe <robbejeffrey@gmail.com>
2024-05-04 17:37:55 +0200

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hello World!</p>"
+    return "<p>Bedankt voor de opleiding Peter!</p>"
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
